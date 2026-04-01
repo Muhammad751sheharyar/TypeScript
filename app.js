b66ab894f7f3d66516574304cee1020225790da0
Muhammad751sheharyar <sheharyar117794@gmail.com>
2026-04-01 12:50:07 +0500

    object

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 "use strict";
+// const firstName:string= "Ali";
+// console.log(firstName);
 Object.defineProperty(exports, "__esModule", { value: true });
-const firstName = "Ali";
-console.log(firstName);
+let Information = {
+    Name: "Sheharyar",
+    Age: 18,
+    Cnic: 432144234234
+};
+console.log(Information);
--- app.ts
+++ app.ts
@@ -1,2 +1,15 @@
-const firstName:string= "Ali";
-console.log(firstName);
+// const firstName:string= "Ali";
+// console.log(firstName);
+
+
+
+let Information: {
+    Name: string,
+    Age: number,
+    Cnic: number,
+}={
+    Name:"Sheharyar",
+    Age:18,
+    Cnic:432144234234
+}
+console.log(Information);
