7baad71782c4180cbc39d5021d7a49a489a1999b
Muhammad751sheharyar <sheharyar117794@gmail.com>
2026-04-03 12:04:49 +0500

    again

--- dist/app.js
+++ dist/app.js
@@ -8,3 +8,19 @@ let Information = {
     Cnic: 432144234234
 };
 console.log(Information);
+// interface IManagment {
+//     staffId?: number | string,
+//     staffName?: string
+// }
+// interface IStudent {
+//     studentId?: number | string,
+//     studentName?: string
+// }
+// type interset = IManagment & IStudent
+// let Data:interset={
+//     studentId:1234,
+//     studentName:"Danish",
+//     staffId:1001,
+//     staffName:"Gulzar",
+// }
+// console.log(Data)
