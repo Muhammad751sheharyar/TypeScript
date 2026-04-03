30afeb84db57d7003739090031dd374156cc75b5
Muhammad751sheharyar <sheharyar117794@gmail.com>
2026-04-03 12:43:25 +0500

    type

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,38 @@
-const firstName:string= "Ali";
-console.log(firstName);
+// const firstName:string= "Ali";
+// console.log(firstName);
 
 
-let Information: {
-    Name: string,
-    Age: number,
-    Cnic: number,    
-}={
-    Name:"Sheharyar",
-    Age:18,
-    Cnic:432144234234
+// let Information: {
+//     Name: string,
+//     Age: number,
+//     Cnic: number,    
+// }={
+//     Name:"Sheharyar",
+//     Age:18,
+//     Cnic:432144234234
+// }
+// console.log(Information);
+
+
+
+
+interface IManagment {
+    staffId?: number | string,
+    staffName?: string
 }
-console.log(Information);
+interface IStudent {
+    studentId?: number | string,
+    studentName?: string
+}
 
 
+type interset = IManagment & IStudent
 
 
-// interface IManagment {
-//     staffId?: number | string,
-//     staffName?: string
-// }
-// interface IStudent {
-//     studentId?: number | string,
-//     studentName?: string
-// }
-
-
-// type interset = IManagment & IStudent
-
-
-// let Data:interset={
-//     studentId:1234,
-//     studentName:"Danish",
-//     staffId:1001,
-//     staffName:"Gulzar",
-// }
-// console.log(Data)
+let Data:interset={
+    studentId:1234,
+    studentName:"Danish",
+    staffId:1001,
+    staffName:"Gulzar",
+}
+console.log(Data)
